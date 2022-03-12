465634fbd4b73b8c4b06590d7a0a2a8fbbce13e7
EC2 Default User <ec2-user@ip-172-31-14-104.ec2.internal>
2022-03-12 14:36:55 +0000

    [Add]更新機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,34 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
+  private #ストロングパラメータ
+
+  def book_params
+    params.require(:book).permit(:本のタイトル,:感想)
   end
 end
